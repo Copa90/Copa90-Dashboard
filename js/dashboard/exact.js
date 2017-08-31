a40618f4697dd95d1bc33c21018aadf62c6a87b4
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-08-31 18:26:15 +0430

    Change URLs to Production URLs

--- js/dashboard/exact.js
+++ js/dashboard/exact.js
@@ -84,13 +84,13 @@ function ensuranceOperation(callback) {
 	})
 }
 
-// var coreEngine_url 	= "http://185.105.186.68:4000/api/"
-// var zarinPal_url 		= "http://185.105.186.68:4010/api/"
-// var coreURL 				= 'http://copa90.ir/'
-
-var coreEngine_url 	= "http://127.0.0.1:4000/api/"
-var zarinPal_url 		= "http://127.0.0.1:4010/api/"
+var coreEngine_url 	= "http://185.105.186.68:4000/api/"
+var zarinPal_url 		= "http://185.105.186.68:4010/api/"
 var coreURL 				= 'http://copa90.ir/'
+
+// var coreEngine_url 	= "http://127.0.0.1:4000/api/"
+// var zarinPal_url 		= "http://127.0.0.1:4010/api/"
+// var coreURL 				= 'http://copa90.ir/'
 
 $(document).ready(function () {
 
